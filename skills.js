df33012589f4df0cb05e49e55b822cc035591edb
fares ibn othmen <150443259+fares-ben-othman@users.noreply.github.com>
2024-05-26 09:57:56 +0100

    Add files via upload

---
+++ skills.js
@@ -0,0 +1,87 @@
+function monScript() {
+    alert('Le bouton a été pressé !');
+}
+
+var bouton = document.getElementById('skl');
+bouton.addEventListener('click', incrementer);
+function incrementer(){
+    let number1 =document.getElementById("number1");
+  let counter1= 0;
+  setInterval(()=> {
+    if(counter1==85){
+      clearInterval();}
+      else{
+        counter1+=1;
+        number1.innerHTML= counter1 + "%";}},75); 
+
+
+  let number2 =document.getElementById("number2");
+  let counter2= 0;
+  setInterval(()=> {
+    if(counter2==60){
+      clearInterval();}
+      else{
+        counter2+=1;
+        number2.innerHTML= counter2 + "%";}},75); 
+
+
+  let number3 =document.getElementById("number3");
+  let counter3= 0;
+  setInterval(()=> {
+    if(counter3==50){
+      clearInterval();}
+      else{
+        counter3+=1;
+        number3.innerHTML= counter3 + "%";}},75); 
+
+
+  let number4 =document.getElementById("number4");
+  let counter4= 0;
+  setInterval(()=> {
+    if(counter4==75){
+      clearInterval();}
+      else{
+        counter4+=1;
+        number4.innerHTML= counter4 + "%";}},75); 
+
+
+  let number5 =document.getElementById("number5");
+  let counter5= 0;
+  setInterval(()=> {
+    if(counter5==70){
+      clearInterval();}
+      else{
+        counter5+=1;
+        number5.innerHTML= counter5 + "%";}},75); 
+
+
+  let number6 =document.getElementById("number6");
+  let counter6= 0;
+  setInterval(()=> {
+    if(counter6==60){
+      clearInterval();}
+      else{
+        counter6+=1;
+        number6.innerHTML= counter6 + "%";}},75); 
+
+
+  let number7 =document.getElementById("number7");
+  let counter7= 0;
+  setInterval(()=> {
+    if(counter7==50){
+      clearInterval();}
+      else{
+        counter7+=1;
+        number7.innerHTML= counter7 + "%";}},75); 
+
+
+  let number8 =document.getElementById("number8");
+  let counter8= 0;
+  setInterval(()=> {
+    if(counter8==80){
+      clearInterval();}
+      else{
+        counter8+=1;
+        number8.innerHTML= counter8 + "%";}},75); 
+
+}
